62f2e6710637c42d9c6c3acdf53f875cde897139
Brajesh <brjnit2009@gmail.com>
2020-07-29 15:10:26 +0700

    added loader component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import Routes from './Routes';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './Redux/store/configStore';
-import { Provider } from "react-redux";
-import Header from './Component/header';
+import { Provider, useSelector } from "react-redux";
+import Loader from './Component/loader';
 
 function App() {
-  
   return (
     <div>
       <Provider store={store}>
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Route, Redirect, Switch, withRouter } from "react-router-dom";
 import Home from "../Screen/Home";
 import Login from "../Screen/Login";
-
+import { useSelector } from "react-redux";
+import Loader from '../Component/loader';
 const ProtectedRoute = ({ auth, component: Component, ...rest }) => {
     return (
         <Route
@@ -15,6 +16,10 @@ const ProtectedRoute = ({ auth, component: Component, ...rest }) => {
 
 const Routes = () => {
     const [isAuthenticated, authenticate] = useState(false)
+    const isLaoding = useSelector(state => state.loading.showLoader);
+    if (isLaoding) {
+            return (<Loader/>)
+    }
     return (
         <Switch>
             <Route exact path="/" component={Login}/>
